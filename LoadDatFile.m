2dfc0f900b0784108024cef8488eac86d9c57198
Zachary Roth <zjroth@gmail.com>
2013-03-15 01:58:48 -0500

    Minor changes to `LoadDatFile`
    * Whitespace changes
    * Removed unnecessary return-statement

--- LoadDatFile.m
+++ LoadDatFile.m
@@ -1,30 +1,30 @@
-function [eeg] = LoadDatFile(filename, chID, startRead, chunkLength, nChannelsTot)
-
+function eeg = LoadDatFile(filename, chID, startRead, chunkLength, nChannelsTot)
   % set up array for Dat
   eeg = zeros(length(chID), chunkLength);
 
   % load dat data
   fp = fopen([filename '.dat'], 'r');
+
+  % The `2` here indicates that each datum is 2 bytes.
   status = fseek(fp, startRead*nChannelsTot*2, 'bof');
 
   buffersize = 2^10; %2^18;
 
-  eeg=zeros(length(chID),chunkLength);
-  N_EL=0;
-  numelm=0;
+  eeg = zeros(length(chID), chunkLength);
+  N_EL = 0;
+  numelm = 0;
 
   while N_EL < chunkLength
-    [data,count] = fread(fp,[nChannelsTot,buffersize],'int16');
-    numelm = count/nChannelsTot;
+    [data, count] = fread(fp, [nChannelsTot, buffersize], 'int16');
+    numelm = count / nChannelsTot;
     if numelm > 0 % Kenji modified 061009.Otherwise if numelm == 0 an error occur.
-      eeg(:,N_EL+1:N_EL+numelm) = data(chID,:);
-      N_EL = N_EL+numelm;
-    else break;
+      eeg(:, N_EL+1 : N_EL+numelm) = data(chID, :);
+      N_EL = N_EL + numelm;
+    else
+      break;
     end
   end
   fclose(fp);
 
-  eeg = eeg(1:chunkLength);
-
-  return;
+  eeg = eeg(1 : chunkLength);
 end
