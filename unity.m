beef37b927fa5ce9790e96c5b176ac911532059e
zjroth <zjroth@gmail.com>
2013-06-10 11:38:44 -0500

    Unity function now computes z-scores

--- unity.m
+++ unity.m
@@ -9,11 +9,9 @@ function U = unity(A,exclude)
         for ii = 1:size(exclude,1)
             ii_ok(exclude(ii,1):exclude(ii,2)) = 0;
         end
-        %     stdA = std(A(find(ii_ok)));
-        stdA = median(abs(A(find(ii_ok)))/sqrt(2)/0.6745);
+        stdA = std(A(find(ii_ok)));
     else
-        %     stdA = std(A);
-        stdA = median(abs(A)/sqrt(2)/0.6745);
+        stdA = std(A);
     end
 
     U = (A - repmat(meanA,size(A,1),1))./repmat(stdA,size(A,1),1);
