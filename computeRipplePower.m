e2602e22de210086a4cc1ad4877c7b3b922bfada
zjroth <zjroth@gmail.com>
2013-06-10 11:33:43 -0500

    Use spectrogram method for ripple-band "filtering"

--- computeRipplePower.m
+++ computeRipplePower.m
@@ -1,40 +1,86 @@
 %------------------------------------------------------------------------------
 % Usage:
-%    ripplePower = computeRipplePower()
+%    rplPower = computeRipplePower()
 % Description:
 %    Here is Eva's original comment for this code: "detect ripple power".
 % Arguments:
-%    rippleLfp
+%    lfp
 %       The LFP to detect the "ripple power" of
-%    freqRange
-%       A vector of length 2 containing the allowed range of frequencies (in
-%       Hertz) for a ripple, for example `[90, 180]` to allow 90Hz to 180Hz.
+%    sampleRate
+%       The rate at which the LFP was sampled
 % Returns:
-%    ripplePower
+%    rplPower
 %       .
 %------------------------------------------------------------------------------
-function ripplePower = computeRipplePower(rippleLfp, freqRange, sampleRate, ...
-                                          smoothingFilter)
-    % Some parameters that were moved here from `ripdetect_sev.m`.
-    filtOrder = 500;  % filter order has to be even; .. the longer the more
-                      % selective, but the operation will be linearly slower
-                      % to the filter order
-    filtOrder = ceil(filtOrder/2)*2;           %make sure filter order is even
-    avgFiltOrder = 501; % do not change this... length of averaging filter
+function rplPower = computeRipplePower(lfp, passBand, sampleRate)
+    % The frequency ranges (in Hertz) for theta waves and ripple waves.
+    rplFreqRange = passBand;
+    % thetaFreqRange = [6, 10];
 
-    % Retrieve the low- and high-band frequencies from the provided frequency
-    % range.
-    lowband = freqRange(1);
-    highband = freqRange(2);
+    %
+    rplWindow = 0.25;
+    % thetaWindow = 0.5;
 
-    % Create a band-pass filter
-    firfiltb = fir1(filtOrder, [lowband / sampleRate*2, highband / sampleRate*2]);
-    avgfiltb = ones(avgFiltOrder, 1) / avgFiltOrder;
+    % Compute the actual short-time Fourier transforms for the desired
+    % frequencies.
+    [rplSpect, rplTimes, rplFreqs] = MTSpectrogram( ...
+        lfp         ,                               ...
+        'frequency' , sampleRate,                   ...
+        'window'    , rplTimeWindow,                ...
+        'step'      , (1 / sampleRate),             ...
+        'range'     , rplFreqRange                  ...
+    );
 
-    % Filter the LFP with the band-pass filter, compute its power (?), scale the
-    % resultant signal (?), and smooth the scaled signal.
-    rip = Filter0(firfiltb, rippleLfp); % filtering
-    rip = rip.^2; % filtered * filtered >0
-    rip = unity(rip);
-    ripplePower = conv(rip, smoothingFilter, 'same');
+    % [thetaSpect, thetaTimes, thetaFreqs] = MTSpectrogram( ...
+    %     lfp         ,                                     ...
+    %     'frequency' , sampleRate,                         ...
+    %     'window'    , thetaTimeWindow,                    ...
+    %     'step'      , (1 / sampleRate),                   ...
+    %     'range'     , thetaFreqRange                      ...
+    % );
+
+    % We want to account for the fact that "the power spectrum of intracortical
+    % local field potential (LFP) often scales as 1/f^2". Note that this fact
+    % seems not to hold for frequencies below about 10 Hz, which means that
+    % we should probably be using a different method for the theta spectrogram.
+    rplSpect = scaleLfpSpectrogram(rplSpect, rplFreqs);
+    % scaledThetaSpect = scaleLfpSpectrogram(thetaSpect, thetaFreqs);
+
+    % To account for some artifacts due to computing a spectrogram, we want
+    % to smooth across frequencies in the spectrograms.
+    smoothingFilter = gausswin(20);
+    smoothingFilter = smoothingFilter / sum(smoothingFilter);
+    rplSpect = conv2(rplSpect, smoothingFilter, 'same');
+
+    % We want to combine the transforms for each window. For now, we will take
+    % the maximum value found for each frequency range (at a given time).
+    rplMax = max(rplSpect, [], 1);
+    % thetaMax = max(thetaSpect, [], 1);
+
+    % We probably want to use `interp1` here. This in combination with the time
+    % data returned by the `spectrogram` calls and with the length of
+    % `lfp` should allow us to compute R(t) = S_theta(t) / S_ripple(t) for
+    % the desired values of t, except for maybe values near the ends of the
+    % vectors.
+    rplPower = (rplMax - mean(rplMax)) / std(rplMax);
+end
+
+%------------------------------------------------------------------------------
+% Usage:
+%    scaled = scaleLfpSpectrogram(spect)
+% Description:
+%    .
+% Arguments:
+%    spect
+%       .
+% Returns:
+%    scaled
+%       .
+%------------------------------------------------------------------------------
+function scaled = scaleLfpSpectrogram(spect, freqs)
+    scaled = spect;
+
+    for i = 1:length(freqs)
+        scaled(i, :) = log(scaled(i, :)) * log(freqs(i));
+    end
 end
