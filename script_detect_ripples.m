08ba513982d2041621d3ebe326defb0b47cef1fe
zjroth <zjroth@gmail.com>
2013-06-12 00:23:18 -0500

    Update ripple-detection script to pass data
    ...instead of passing in a filename.

--- script_detect_ripples.m
+++ script_detect_ripples.m
@@ -1,8 +1,23 @@
-% This is the name of the file.
+% Some initialization stuff...
 filename = ['/home/s-zroth1/data/Eva-new-maze-2013/' ...
             'A543-20120422-01/A543-20120422-01.sev'];
-chIDs      = [34, 35, 46];
+channels   = [34, 35, 46];
 sampleRate = 2e4;
 nChannels  = 256;
 
-[spw, dat, fShp, fRip] = ripdetect_sev(filename, sampleRate, nChannels, chIDs);
+% Load the data.
+disp('Loading the data...');
+listing = dir(filename);
+Nsamples = listing.bytes / (2 * nChannels);  % sec
+
+nch = 0;
+for currChannel = channels
+    fidSev = fopen(filename, 'r');
+    startRead = (currChannel - 1) * Nsamples * 2;
+
+    nch = nch + 1;
+    dat(:, nch) = LoadSevFile(fidSev, startRead, Nsamples);
+end
+
+% Find the ripples
+[spw, fShp, fRip] = ripdetect_sev(dat, sampleRate);
