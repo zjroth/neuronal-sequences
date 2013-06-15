e2a014d288fa8179478b022f6595df4f121736f9
zroth <zroth@ubuntu.(none)>
2013-06-15 13:58:17 -0500

    Added a script to initialize the workspace

---
+++ script_init_workspace.m
@@ -0,0 +1,9 @@
+% Ensure that this directory is the current directory.
+[folder, ~, ~] = fileparts(mfilename('fullpath'));
+cd(folder);
+
+% Add the appropriate folders to the path.
+addpath('./matlab-incremented');
+%addpath('./comparison/')
+addpath(genpath('./chronux/chronux/'))
+addpath(genpath('./fma-toolbox/'))
