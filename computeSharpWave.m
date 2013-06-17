c3dcd581cde58567a33aa49a84c42bcb08ef1b1a
zroth <zroth@ubuntu.(none)>
2013-06-17 11:05:13 -0500

    Remove normalization from sharp-wave computation

--- computeSharpWave.m
+++ computeSharpWave.m
@@ -21,13 +21,5 @@ function sharpWave = computeSharpWave(lowLfp, highLfp, smoothingFilter)
     shp = (highLfp - mean(highLfp)) - (lowLfp - mean(lowLfp));
 
     % Smooth the sharp-wave.
-    fShp = conv(shp, smoothingFilter, 'same');
-
-    % Pass the smoothed sharp-wave through `unity`. I believe that `unity` is
-    % supposed to take care of scaling issues, but I'm not sure. Eva's
-    % original note for this line is simply "z-score", suggesting to me that
-    % this is supposed to be an approximation for the z-score for each data
-    % point; however, a value dependent on the median is used rather than the
-    % standard deviation in the computation of this purported z-score.
-    sharpWave = unity(fShp);
+    sharpWave = conv(shp, smoothingFilter, 'same');
 end
