8d6978d21a00835c56aa88366a84d226f7fcc963
zjroth <zjroth@gmail.com>
2013-07-01 17:07:23 -0500

    Updated computation of ripple spectrograms
    * Save the appropriate data if an output file name is provided.
    * Set the default value of the window step only after we have the actual
      value of the LFP's sample rate.
    * Updated documentation and comments
    * Modified variable names

--- rippleSpectrogram.m
+++ rippleSpectrogram.m
@@ -12,6 +12,23 @@
 %    lfp
 %       The LFP for which we are computing a spectrogram
 %
+% OPTIONAL PARAMETERS:
+%
+%    lfpSampleRate (default: 2e4)
+%       The sample rate (in Hertz) of the provided LFP
+%
+%    frequencyRange (default: [90, 180])
+%       The frequency range (in Hertz) for which the spectrogram will be
+%       computed
+%
+%    windowStep (default: 1 / lfpSampleRate)
+%       The step (in seconds) between the centers of successive windows used in
+%       the spectrogram computation
+%
+%    windowWidth (default: 0.25)
+%       The width (in seconds) of the window to use in the spectrogram
+%       computation.
+%
 % RETURNS:
 %
 %    spect
@@ -23,20 +40,23 @@
 %    frequencies
 %       The frequencies (in Hertz) for which the spectrogram contains data
 %------------------------------------------------------------------------------
-function [spect, times, frequencies] = rippleSpectrogram(lfp, varargin)
+function [spect, spectTimes, spectFrequencies] = rippleSpectrogram(lfp, varargin)
     %=======================================================================
     % Default optional parameter values
     %=======================================================================
 
     % All times are in seconds, and all frequencies are in Hertz.
-    sampleRate = 2e4;
+    lfpSampleRate = 2e4;
     frequencyRange = [90, 180];
     windowWidth = 0.25;
-    windowStep = (1 / sampleRate);
 
     % Replace the default values with any values that were passed to
     % the function.
     parseNamedParams();
+
+    if ~exist('windowStep', 'var')
+        windowStep = (1 / lfpSampleRate);
+    end
 
     %=======================================================================
     % Actual computations
@@ -44,19 +64,25 @@ function [spect, times, frequencies] = rippleSpectrogram(lfp, varargin)
 
     % Compute the actual short-time Fourier transforms for the desired
     % frequencies.
-    [spect, times, frequencies] = MTSpectrogram( ...
-        lfp         ,                            ...
-        'frequency' , sampleRate,                ...
-        'window'    , windowWidth,               ...
-        'step'      , windowStep,                ...
-        'range'     , frequencyRange             ...
+    [spect, spectTimes, spectFrequencies] = MTSpectrogram( ...
+        lfp         ,                                      ...
+        'frequency' , sampleRate,                          ...
+        'window'    , windowWidth,                         ...
+        'step'      , windowStep,                          ...
+        'range'     , frequencyRange                       ...
     );
 
     % We want to account for the fact that "the power spectrum of intracortical
-    % local field potential (LFP) often scales as 1/f^2". Note that this fact
-    % seems not to hold for frequencies below about 10 Hz, which means that
-    % we should probably be using a different method for the theta spectrogram.
-    for i = 1 : length(frequencies)
-        spect(i, :) = log(spect(i, :)) * log(frequencies(i));
+    % local field potential (LFP) often scales as 1/f^2" (for frequencies above
+    % about 10 Hertz).
+    for i = 1 : length(spectFrequencies)
+        spect(i, :) = log(spect(i, :)) * log(spectFrequencies(i));
+    end
+
+    % Save the data if requested.
+    if exist('outputFile', 'var')
+        save(outputFile, 'frequencyRange', 'lfp', 'spect', 'spectTimes', ...
+            'spectFrequencies', 'lfpSampleRate', 'windowWidth', ...
+            'windowStep');
     end
 end
