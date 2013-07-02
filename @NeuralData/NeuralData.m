f1ea608545f88b70a29e81f7a2d0d24c2c283ba2
zjroth <zjroth@gmail.com>
2013-07-02 10:17:30 -0500

    Updated NeuralData constructor and method list

--- @NeuralData/NeuralData.m
+++ @NeuralData/NeuralData.m
@@ -1,7 +1,8 @@
 %
 % DESCRIPTION:
 %
-%    Provide convenient access to neural data related to ripple detection.
+%    Provide convenient access to neural data and methods related to ripple
+%    detection.
 %
 % PROPERTIES:
 %
@@ -41,8 +42,10 @@ classdef NeuralData
         %       .
         %
         %---------------------------------------------------------------
-        function this = NeuralData(folder)
-            load([folder '.mat']);
+        function this = NeuralData(filename)
+            [filePath, baseFileName, ~] = fileparts(filename);
+
+            load(fullfile(filePath, [baseFileName '_BehavElectrDataLFP.mat']));
 
             this.Clu = Clu;
             this.Laps = Laps;
@@ -53,36 +56,24 @@ classdef NeuralData
     end
 
     methods (Access = public)
-        tms = ripples(this)
-        tms = lfp(this)
-        % clln = CodeComplex(this)
+        clusterSpikeTimes = groupSpikes(this)
+
+        rate = sampleRate(this)
+
+        % Method ideas:
+        %    mtx = ripples(this, channels)
+        %       This is fast to compute. Don't provide functionality for
+        %       saving unless we have a specific reason to. I'm thinking that
+        %       this should just be the "DetectRipples" function; move it in
+        %       here.
         %
-        % cllnDecoded = Decode(this, clln)
+        %    lfpTs = lfp(this, channels)
+        %        These are surprisingly slow to extract from the .dat file.
+        %        Should I provide functionality for saving? Probably not. I'm
+        %        thinking of using the FMAToolbox function "LoadBinary".
         %
-        % cllnDecoded = DecodeMAP(this, clln)
-        %
-        % d = Distance(this, x, y)
-        %
-        % gph = Graph(this)
-        %
-        % gph = CodewordGraph(this, tol)
-        %
-        % iLength = Length(this)
-        %
-        % cllnRand = RandomSample(this, iSize)
-        %
-        % r = Rate(this)
-        %
-        % SetDistribution(this, cvDistribution)
-        %
-        % SetMetric(this, fcnHandle)
-        %
-        % codeNew = Shuffle(this)
-        %
-        % iSize = Size(this)
-        %
-        % s = Sparsity(this)
-        %
-        % mtx = ToMatrix(this)
+        % Other ideas:
+        %    If we do need to store ripples for some reason, use files of the
+        %    form `ch-<low>-<main>-<high>.rpl` in the subfolder "computed".
     end
 end
