9febd0851d73d76a5dc79f8925c6bd8879ec7a3a
zjroth <zjroth@gmail.com>
2013-08-02 17:23:49 -0500

    Store spikes as time data when grouping spikes

--- @NeuralData/groupSpikes.m
+++ @NeuralData/groupSpikes.m
@@ -1,12 +1,14 @@
 % function clusterSpikeTimes = groupSpikes(this)
-function clusterSpikeTimes = groupSpikes(this)
+function cellSpikeTimes = groupSpikes(this)
     % Build a matrix of unique cell/shank combinations.
     numClusters = max(this.Spike.totclu);
-    clusterSpikeTimes = cell(numClusters, 1);
+    cellSpikeTimes = cell(numClusters, 1);
 
+    % Group all of the clusters that are labeled the same. Also, convert from
+    % index data to time data.
     for i = 1 : numClusters
-        clusterSpikeTimes{i} = this.Spike.res(this.Spike.totclu == i);
+        cellSpikeTimes{i} = this.Spike.res(this.Spike.totclu == i) / sampleRate(this);
     end
 
-    this.current.spikeTrains = clusterSpikeTimes;
+    this.current.spikeTrains = cellSpikeTimes;
 end
