e348929338a3fea328397b9e0f9d7f218aa5022b
zjroth <zjroth@gmail.com>
2013-08-14 17:51:06 -0500

    Add computeMu

---
+++ sequences/computeMu.m
@@ -0,0 +1,7 @@
+function mtxMu = computeMu(mtxM, vN)
+    mtxDenoms = vN(:) * vN(:)';
+    mtxDenomsNonZero = sparse(mtxDenoms ~= 0);
+    mtxMu = zeros(size(mtxM));
+    mtxMu(mtxDenomsNonZero) = mtxM(mtxDenomsNonZero) ./ ...
+        mtxDenoms(mtxDenomsNonZero) - 0.5;
+end
