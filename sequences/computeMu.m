6b35094981eaed3da27559c76a28044ba4ba3a67
zjroth <zjroth@gmail.com>
2013-08-15 12:10:22 -0500

    Add comfort parentheses in `computeMu`

--- sequences/computeMu.m
+++ sequences/computeMu.m
@@ -2,6 +2,6 @@ function mtxMu = computeMu(mtxM, vN)
     mtxDenoms = vN(:) * vN(:)';
     mtxDenomsNonZero = sparse(mtxDenoms ~= 0);
     mtxMu = zeros(size(mtxM));
-    mtxMu(mtxDenomsNonZero) = mtxM(mtxDenomsNonZero) ./ ...
-        mtxDenoms(mtxDenomsNonZero) - 0.5;
+    mtxMu(mtxDenomsNonZero) = (mtxM(mtxDenomsNonZero) ./ ...
+        mtxDenoms(mtxDenomsNonZero)) - 0.5;
 end
