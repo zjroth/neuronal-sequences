a18c2cbbdfcf81157fe587caab3daca37cfcbf4a
zjroth <zjroth@gmail.com>
2013-08-16 12:08:22 -0500

    Never produce a rho value that is `NaN`
    Also, add the function signature as the documentation comment to
    `computeRho`.

--- sequences/computeRho.m
+++ sequences/computeRho.m
@@ -1,3 +1,4 @@
+% dRho = computeRho(mtxMu1, mtxMu2, vCommonNeurons)
 function dRho = computeRho(mtxMu1, mtxMu2, vCommonNeurons)
     % Restrict to the submatrices that correspond to the neurons that the
     % corresponding sequences share.
@@ -9,6 +10,15 @@ function dRho = computeRho(mtxMu1, mtxMu2, vCommonNeurons)
     dDotProduct = full(sum(sum(mtxLocalMu1 .* mtxLocalMu2)));
     dNorm1 = sqrt(sum(sum(mtxLocalMu1 .* mtxLocalMu1)));
     dNorm2 = sqrt(sum(sum(mtxLocalMu2 .* mtxLocalMu2)));
+    dDenom = dNorm1 * dNorm2;
 
-    dRho = dDotProduct / (dNorm1 * dNorm2);
+    % Account for the case that one of the restricted matrices is a zero-matrix.
+    % This can happen even if the corresponding sequences have common neurons
+    % since the mu-matrices are stored as upper-triangular matrices despite the
+    % fact that the full matrix of mu values would not be symmetric.
+    if dDenom ~= 0
+        dRho = dDotProduct / dDenom;
+    else
+        dRho = 0;
+    end
 end
