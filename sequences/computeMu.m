3e59c68365e22bf23ec9a5cc6351a6e8066a30b5
zjroth <zjroth@gmail.com>
2013-08-16 12:23:53 -0500

    Update/fix `computeMu`
    * Return only the strictly upper-triangular portion of the matrix of mu
      values.
    * Add comments and a documentation string (simply the function
      signature).

--- sequences/computeMu.m
+++ sequences/computeMu.m
@@ -1,6 +1,15 @@
+% mtxMu = computeMu(mtxM, vN)
 function mtxMu = computeMu(mtxM, vN)
+    % The value $mu_{ij}$ is zero unless both neurons $i$ and $j$ appear in the
+    % corresponding sequence $s$. If both $i$ and $j$ appear in the sequence,
+    % then $\mu_{ij}(s) = m_{ij}(s) / (n_i n_j) - 1/2$, which is defined since
+    % $n_i, n_j \neq 0$.
+
+    % The code below simply computes all mu values according to the above
+    % formula. Since the resultant matrix will likely be sparse, we can speed up
+    % computation by using sparse matrices.
     mtxDenoms = vN(:) * vN(:)';
-    mtxDenomsNonZero = sparse(mtxDenoms ~= 0);
+    mtxDenomsNonZero = sparse(triu(mtxDenoms ~= 0, 1));
     mtxMu = zeros(size(mtxM));
     mtxMu(mtxDenomsNonZero) = (mtxM(mtxDenomsNonZero) ./ ...
         mtxDenoms(mtxDenomsNonZero)) - 0.5;
