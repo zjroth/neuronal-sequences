598f76db5c443d48331f0b420efe18c3d753ab61
zjroth <zjroth@gmail.com>
2013-08-16 12:27:17 -0500

    Minor updates to `computeM`

--- sequences/computeM.m
+++ sequences/computeM.m
@@ -1,19 +1,19 @@
-function M = computeM(vSeq, nMax)
+% mtxM = computeM(vSeq, nMax)
+function mtxM = computeM(vSeq, nMax)
     % Find the unique elements of the sequence.  This is much faster than
     % calling `unique(vSeq)`.
     vUnique = zeros(1, nMax);
     vUnique(vSeq) = 1;
     vUnique = find(vUnique);
-    nElts = length(vUnique);
 
     % Build a matrix whose row `i` is the indicator vector of where `i` fires in
-    % the sequence. This is an `nElts` by `length(vSeq2)` matrix.
+    % the sequence.
     mtxSupports = bsxfun(@eq, vUnique(:), vSeq(:)');
 
     % Compute the small submatrix of M where non-zeros can exist.
-    mtxM = triu(cumsum(mtxSupports, 2) * mtxSupports', 1);
+    mtxMRestricted = triu(cumsum(mtxSupports, 2) * mtxSupports', 1);
 
     % Fill in a full-size matrix M with the appropriate non-zero values.
-    M = sparse([], [], [], nMax, nMax, nnz(mtxM));
-    M(vUnique, vUnique) = mtxM;
+    mtxM = sparse([], [], [], nMax, nMax, nnz(mtxMRestricted));
+    mtxM(vUnique, vUnique) = mtxMRestricted;
 end
