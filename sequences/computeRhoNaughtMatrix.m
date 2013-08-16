765632852133b70f6df49e91144438e73d6c8a47
zjroth <zjroth@gmail.com>
2013-08-16 12:45:41 -0500

    Add `computeRhoNaughtMatrix`

---
+++ sequences/computeRhoNaughtMatrix.m
@@ -0,0 +1,18 @@
+% mtxRhoNaught = computeRhoNaughtMatrix(cellSeqs, nTrials, dProb)
+function mtxRhoNaught = computeRhoNaughtMatrix(cellSeqs, nTrials, dProb)
+    % Some initialization.
+    nSeqs = length(cellSeqs);
+    arrRhoMatrices = zeros(nSeqs, nSeqs, nTrials);
+
+    % For each trial...
+    parfor i = 1 : nTrials
+        % ...compute the rho matrix for a collection of shuffled sequences.
+        cellShuffled = cellfun(@shuffle, cellSeqs, 'UniformOutput', false);
+        arrRhoMatrices(:, :, i) = computeRhoMatrix(cellShuffled, 0);
+    end
+
+    % Sort the rho values for each pair of sequences (individually), then pick
+    % out the matrix of rho_naught values to return.
+    arrRhoMatrices = sort(abs(arrRhoMatrices), 3);
+    mtxRhoNaught = arrRhoMatrices(:, :, floor(nTrials * dProb));
+end
