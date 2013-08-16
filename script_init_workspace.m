7da705dff204d532183e7411cab89df209302d66
zjroth <zjroth@gmail.com>
2013-08-16 13:09:07 -0500

    Updates to workspace initialization script

--- script_init_workspace.m
+++ script_init_workspace.m
@@ -1,5 +1,7 @@
 % Enable parallel processing
-% matlabpool('open');
+if matlabpool('size') == 0
+    matlabpool('open');
+end
 
 % Ensure that this directory is the current directory.
 [folder, ~, ~] = fileparts(mfilename('fullpath'));
@@ -7,7 +9,11 @@ cd(folder);
 
 % Add the appropriate folders to the path.
 addpath('./matlab-incremented');
+addpath('./neural-codeware');
+addpath('./matlab-cliquer');
 %addpath('./comparison/')
 addpath('./xmltree/');
 addpath(genpath('./chronux/chronux/'))
 addpath(genpath('./fma-toolbox/'))
+
+addpath('./sequences/');
