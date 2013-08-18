3c0e99dbb62f237024737a2c6765ab0f90d5a54d
zjroth <zjroth@gmail.com>
2013-08-18 14:37:51 -0500

    Saving rho matrices as shuffle trials are run

--- sequences/runShuffleTrials.m
+++ sequences/runShuffleTrials.m
@@ -1,13 +1,32 @@
-%  arrRhoMatrices = runShuffleTrials(cellSeqs, nTrials)
-function arrRhoMatrices = runShuffleTrials(cellSeqs, nTrials)
+% arrRhoMatrices = runShuffleTrials(cellSeqs, nTrials, strFolder)
+function arrRhoMatrices = runShuffleTrials(cellSeqs, nTrials, strFolder)
     % Some initialization.
     nSeqs = length(cellSeqs);
     arrRhoMatrices = zeros(nSeqs, nSeqs, nTrials);
+
+    % Determine whether or not we should be saving the individual rho matrices
+    % to files in a specified folder.
+    bSaveFiles = exist('strFolder', 'var');
 
     % For each trial...
     parfor i = 1 : nTrials
         % ...compute the rho matrix for a collection of shuffled sequences.
         cellShuffled = cellfun(@shuffle, cellSeqs, 'UniformOutput', false);
-        arrRhoMatrices(:, :, i) = computeRhoMatrix(cellShuffled, 0);
+        mtxRho = computeRhoMatrix(cellShuffled, 0);
+
+        % Save the files if requested.
+        if bSaveFiles
+            saveRho([strFolder 'trial-' num2str(i)], mtxRho)
+        end
+
+        % Add this matrix to the return variable.
+        arrRhoMatrices(:, :, i) = mtxRho;
     end
+end
+
+% A function for saving within a `parfor` loop. Replacing the invocation of this
+% function with the one line that it contains will cause a transparency
+% violation error.
+function saveRho(strFile, mtxRho)
+    save(strFile, '-v7.3', 'mtxRho');
 end
