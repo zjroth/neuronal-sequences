462a90e2ffec68fe34511c10844b20c4932640ad
zjroth <zjroth@gmail.com>
2013-08-27 11:28:53 -0500

    Update `computeN`: now takes row or column vector

--- sequences/computeN.m
+++ sequences/computeN.m
@@ -2,6 +2,6 @@ function N = computeN(vSeq, nMax)
     if isempty(vSeq)
         N = sparse(nMax, 1);
     else
-        N = sparse(accumarray(vSeq, 1, [nMax, 1]));
+        N = sparse(accumarray(vSeq(:), 1, [nMax, 1]));
     end
 end
