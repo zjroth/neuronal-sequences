874e84d6fd2fcae5a2866a2c90ca412302487d7d
zjroth <zjroth@gmail.com>
2013-08-27 13:34:38 -0500

    Update `computeRho`
    Ensure that only the upper-triangular portion of the matrix of mu values
    is used when computing rho.

--- sequences/computeRho.m
+++ sequences/computeRho.m
@@ -1,15 +1,16 @@
 % dRho = computeRho(mtxMu1, mtxMu2, vCommonNeurons)
 function dRho = computeRho(mtxMu1, mtxMu2, vCommonNeurons)
     % Restrict to the submatrices that correspond to the neurons that the
-    % corresponding sequences share.
-    mtxLocalMu1 = mtxMu1(vCommonNeurons, vCommonNeurons);
-    mtxLocalMu2 = mtxMu2(vCommonNeurons, vCommonNeurons);
+    % corresponding sequences share, and extract the upper-triangular portion
+    % as a vector.
+    vMu1 = triuVals(mtxMu1(vCommonNeurons, vCommonNeurons), 1);
+    vMu2 = triuVals(mtxMu2(vCommonNeurons, vCommonNeurons), 1);
 
     % Now that the above restriction has been performed, the computation of rho
     % is straightforward.
-    dDotProduct = full(sum(sum(mtxLocalMu1 .* mtxLocalMu2)));
-    dNorm1 = sqrt(sum(sum(mtxLocalMu1 .* mtxLocalMu1)));
-    dNorm2 = sqrt(sum(sum(mtxLocalMu2 .* mtxLocalMu2)));
+    dDotProduct = full(sum(vMu1 .* vMu2));
+    dNorm1 = sqrt(sum(vMu1 .* vMu1));
+    dNorm2 = sqrt(sum(vMu2 .* vMu2));
     dDenom = dNorm1 * dNorm2;
 
     % Account for the case that one of the restricted matrices is a zero-matrix.
