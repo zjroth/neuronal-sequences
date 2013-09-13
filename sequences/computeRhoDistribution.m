38f73d3d76f698c947ccd1f386c08aa2481fa9c1
zjroth <zjroth@gmail.com>
2013-09-12 20:07:47 -0500

    Added `computeRhoDistribution`

---
+++ sequences/computeRhoDistribution.m
@@ -0,0 +1,22 @@
+% vDistribution = computeRhoDistribution(vSeq1, vSeq2, nTrials)
+function vDistribution = computeRhoDistribution(vSeq1, vSeq2, nTrials)
+    % Initialize the return variable.
+    vDistribution = zeros(1, nTrials);
+
+    % Find a vector describing which neurons are active in both sequences.
+    nMax = max([vSeq1; vSeq2]);
+    vCoactive = (ind2log(vSeq1, nMax) & ind2log(vSeq2, nMax));
+
+    % The count of neurons in a sequence doesn't change when the sequence is
+    % shuffled.
+    vN1 = computeN(vSeq1, nMax);
+    vN2 = computeN(vSeq2, nMax);
+
+    % For each trial, compute rho between shuffles of each sequence.
+    for i = 1 : nTrials
+        mtxMu1 = computeMu(computeM(shuffle(vSeq1), nMax), vN1);
+        mtxMu2 = computeMu(computeM(shuffle(vSeq2), nMax), vN2);
+
+        vDistribution(i) = computeRho(mtxMu1, mtxMu2, vCoactive);
+    end
+end
