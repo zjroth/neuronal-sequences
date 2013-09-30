0a882c061538bb82784d0f504de7acea7e0f9549
zjroth <zjroth@gmail.com>
2013-09-30 13:55:58 -0500

    Update `NeuralData.getRipples`
    Add comments and update variable names.

--- @NeuralData/getRipples.m
+++ @NeuralData/getRipples.m
@@ -1,11 +1,17 @@
-function ripples = getRipples(this, rippleNums)
+% mtxRipples = getRipples(this, vRippleNums)
+function mtxRipples = getRipples(this, vRippleNums)
+    % If there is no collection of ripples in this object, we must run the
+    % detection method before we can return the collection of ripples.
     if ~isfield(this.current, 'ripples')
         detectRipples(this);
     end
 
-    if ~exist('rippleNums', 'var')
-        rippleNums = (1 : size(this.current.ripples, 1));
+    % If no specific collection of ripples was collected, then we should just
+    % return all of the ripples.
+    if ~exist('vRippleNums', 'var')
+        vRippleNums = (1 : size(this.current.ripples, 1));
     end
 
-    ripples = this.current.ripples(rippleNums, :);
+    % Set the return variable.
+    mtxRipples = this.current.ripples(vRippleNums, :);
 end
