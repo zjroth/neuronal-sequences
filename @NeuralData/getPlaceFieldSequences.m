fdebda1e518576aefbccacd92716f2b5e669e1dd
zjroth <zjroth@gmail.com>
2013-09-30 13:54:59 -0500

    Updated `NeuralData.getPlaceFieldSequences`
    It now calls the method `getPlaceFieldIntervals`.

--- @NeuralData/getPlaceFieldSequences.m
+++ @NeuralData/getPlaceFieldSequences.m
@@ -1,11 +1,5 @@
 % cellSequences = getPlaceCellSequences(this, varargin)
 function cellSequences = getPlaceCellSequences(this, varargin)
-    % (145 < objRatData.pre.Track.xPix) & (objRatData.pre.Track.xPix < 583)
-    % (160 < objRatData.musc.Track.xPix) & (objRatData.musc.Track.xPix < 550)
-
-    vIntervals = (160 < this.Track.xPix) & (this.Track.xPix < 550);
-    mtxIntervals = getIntervals(vIntervals);
-    mtxTimeWindows = mtxIntervals / sampleRate(this);
-
+    mtxTimeWindows = getPlaceFieldIntervals(this);
     cellSequences = getSequences(this, mtxTimeWindows, varargin{:});
 end
