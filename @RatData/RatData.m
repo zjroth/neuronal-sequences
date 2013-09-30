57fe182373d2663b778a76325e9c654a362d46e5
zjroth <zjroth@gmail.com>
2013-09-30 17:26:23 -0500

    Update `RatData` constructor
    * Loads data from a specified folder rather than from a specified rat
      and date.
    * Assumes that additional data is stored in the file `data.mat` inside
      of the specified folder. For instance, the channels to use for ripple
      detection should be placed into this data file.

--- @RatData/RatData.m
+++ @RatData/RatData.m
@@ -40,47 +40,21 @@ classdef RatData < handle
         %       .
         %
         %---------------------------------------------------------------
-        function this = RatData(strRatName, strDate)
-            strBaseDir = '~/data/pastalkova/';
+        function this = RatData(strFolder)
+            objData = matfile([strFolder, 'data.mat'], 'Writable', false);
 
-            if ~strcmp(strRatName, 'A543') || ~strcmp(strDate, '2012-04-22')
-                error(['unknown recording; please modify constructor to use ' ...
-                       'this data']);
-            else
-                cellChannels = { 35, 46, 34 };
-                structParams.interneurons = [49, 66, 77, 90];
+            % Load the data from each of the files.
+            this.pre = NeuralData([strFolder 'pre-muscimol/']);
+            this.musc = NeuralData([strFolder 'muscimol/']);
+            this.post = NeuralData([strFolder 'post-muscimol/']);
 
-                % These neurons were taken from Eva's list:
-                %    left: [91, 74, 109, 125, 36, 83, 30, 123, 79, 86, 117]
-                %    right: [31, 86, 11, 95, 82, 106, 109, 30, 79, 16, ...
-                %            117, 108, 73, 29, 87, 116, 24, 52]
-                %
-                % The following list was constructed by me from the above list.
-                % Neurons were duplicated if multiple place fields seemed to exist,
-                % and neurons were deleted if a place field did not seem to exist.
-                %structParams.placeCellOrdering =  [ ...
-                %    26, 24, 16, 22, 2, 14, 24, 28, 20, 1, 12, 13, 9, 6, 2, 8, ...
-                %    29, 4, 6, 27, 8, 2, 28, 7, 3, 12, 19, 24, 15, 29 ...
-                %];
-                structParams.placeCellOrdering = [ ...
-                    31, 86, 11, 95, 82, 106, 109, 30, 79, 16, 117, 108, 73, ...
-                    29, 87, 116, 24, 52, 91, 74, 109, 125, 36, 83, 30,123, ...
-                    79, 86, 117];
+            % Set the current channels if they are specified in the data
+            % file.
+            if ~isempty(whos(objData, 'vChannels'))
+                cellChannels = num2cell(objData.vChannels);
 
-                % Load the data from each of the files.
-                strFile = [strRatName '/' strDate '/pre-muscimol/'];
-                this.pre = NeuralData([strBaseDir strFile]);
-                this.pre.parameters = structParams;
                 this.pre.setCurrentChannels(cellChannels{:});
-
-                strFile = [strRatName '/' strDate '/muscimol/'];
-                this.musc = NeuralData([strBaseDir strFile]);
-                this.musc.parameters = structParams;
                 this.musc.setCurrentChannels(cellChannels{:});
-
-                strFile = [strRatName '/' strDate '/post-muscimol/'];
-                this.post = NeuralData([strBaseDir strFile]);
-                this.post.parameters = structParams;
                 this.post.setCurrentChannels(cellChannels{:});
             end
         end
