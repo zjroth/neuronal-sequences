09459695b14ee81cf729bb639f807b71f681141d
zjroth <zjroth@gmail.com>
2013-09-30 13:56:47 -0500

    Update `NeuralData.getWheelSequences`
    This now calls `NeuralData.getWheelIntervals`.

--- @NeuralData/getWheelSequences.m
+++ @NeuralData/getWheelSequences.m
@@ -1,7 +1,5 @@
 % cellSequences = getWheelSequences(this, varargin)
 function cellSequences = getWheelSequences(this, varargin)
-    mtxIntervals = getIntervals(logical(this.Laps.WhlSpeedCW));
-    mtxTimeWindows = mtxIntervals / sampleRate(this);
-
+    mtxTimeWindows = getWheelIntervals(this);
     cellSequences = getSequences(this, mtxTimeWindows, varargin{:});
 end
