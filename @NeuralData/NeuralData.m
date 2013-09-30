39521b1d34eab4a100778c24ee99e80557756fb5
zjroth <zjroth@gmail.com>
2013-09-30 12:58:18 -0500

    Remove old comment

--- @NeuralData/NeuralData.m
+++ @NeuralData/NeuralData.m
@@ -143,21 +143,5 @@ classdef NeuralData < handle
 
         modifyRipple(this, nRipple, nStartTime, nEndTime)
         modifyRipples(this, varargin)
-
-        % Method ideas:
-        %    mtx = ripples(this, channels)
-        %       This is fast to compute. Don't provide functionality for
-        %       saving unless we have a specific reason to. I'm thinking that
-        %       this should just be the "DetectRipples" function; move it in
-        %       here.
-        %
-        %    lfpTs = lfp(this, channels)
-        %        These are surprisingly slow to extract from the .dat file.
-        %        Should I provide functionality for saving? Probably not. I'm
-        %        thinking of using the FMAToolbox function "LoadBinary".
-        %
-        % Other ideas:
-        %    If we do need to store ripples for some reason, use files of the
-        %    form `ch-<low>-<main>-<high>.rpl` in the subfolder "computed".
     end
 end
