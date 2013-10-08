124e0dc312d9fa0ec360b368ace66b3c3e5251ec
zjroth <zjroth@gmail.com>
2013-10-08 12:08:18 -0500

    Updates to `computePValues`
    * Specify the number of trials as a parameter.
    * The default p-value is now 1, i.e., not significant.
    * Only compute p-values where there are at least 4 neurons in common. We
      don't care about pairs that overlap in 3 or fewer neurons.
    * Update the function for saving. This now names the saved variable more
      appropriately.

--- sequences/computePValues.m
+++ sequences/computePValues.m
@@ -1,4 +1,4 @@
-% mtxP = computePValues(cellSeqs, strFolder)
+% mtxP = computePValues(cellSeqs, nTrials, strFolder)
 function mtxP = computePValues(cellSeqs, nTrials, strFolder)
     % Determine whether or not we should be saving the individual rho matrices
     % to files in a specified folder.
@@ -13,14 +13,15 @@ function mtxP = computePValues(cellSeqs, nTrials, strFolder)
     nSequences = length(cellSeqs);
 
     % To compute p, we need rho for each pair of sequences. Initialize the
-    % return variable.
+    % return variable under the assumption that all rho values are
+    % insignificant.
     mtxRho = computeRhoMatrix(cellSeqs, 0);
-    mtxP = NaN(nSequences);
+    mtxP = ones(nSequences);
 
     % For each pair of sequences...
     for i = 1 : nSequences - 1
         parfor j = i + 1 : nSequences
-            if mtxNumCoactive(i, j) > 1
+            if mtxNumCoactive(i, j) > 3
                 % ...compute a distribution of rho values for the current
                 % pair of sequences.
                 vDistribution = computeRhoDistribution(cellSeqs{i}, ...
@@ -28,7 +29,7 @@ function mtxP = computePValues(cellSeqs, nTrials, strFolder)
 
                 % Save the distribution if a folder was provided.
                 if bSaveFiles
-                    saveRho([strFolder 'seqs-' num2str(i) '-' num2str(j)], ...
+                    saveDistribution([strFolder 'seqs-' num2str(i) '-' num2str(j)], ...
                             vDistribution);
                 end
 
@@ -47,6 +48,6 @@ end
 % A function for saving within a `parfor` loop. Replacing the invocation of this
 % function with the one line that it contains will cause a transparency
 % violation error.
-function saveRho(strFile, mtxRho)
-    save(strFile, '-v7.3', 'mtxRho');
+function saveDistribution(strFile, vDistribution)
+    save(strFile, '-v7.3', 'vDistribution');
 end
