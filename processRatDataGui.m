d63d3eb9f7f5ee0b7a94b8baa334139853bc87df
zjroth <zjroth@gmail.com>
2013-10-11 14:47:47 -0500

    GUI: add selection of analysis folder

--- processRatDataGui.m
+++ processRatDataGui.m
@@ -21,7 +21,7 @@
 function varargout = processRatDataGui(varargin)
     % Edit the above text to modify the response to help processRatDataGui
 
-    % Last Modified by GUIDE v2.5 09-Oct-2013 15:22:02
+    % Last Modified by GUIDE v2.5 11-Oct-2013 14:45:19
 
     % Begin initialization code - DO NOT EDIT
     gui_Singleton = 1;
@@ -448,4 +448,33 @@ function btnSelectRegions_Callback(hObject, ~, handles)
     % eventdata  reserved - to be defined in a future version of MATLAB
     % handles    structure with handles and user data (see GUIDATA)
     selectRegions(hObject, handles);
+end
+
+% --- Executes on button press in btnBrowseAnalysis.
+function btnBrowseAnalysis_Callback(hObject, eventdata, handles)
+    % hObject    handle to btnBrowseAnalysis (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
+end
+
+function tbxAnalysisFolder_Callback(hObject, eventdata, handles)
+    % hObject    handle to tbxAnalysisFolder (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
+
+    % Hints: get(hObject,'String') returns contents of tbxAnalysisFolder as text
+    %        str2double(get(hObject,'String')) returns contents of tbxAnalysisFolder as a double
+end
+
+% --- Executes during object creation, after setting all properties.
+function tbxAnalysisFolder_CreateFcn(hObject, eventdata, handles)
+    % hObject    handle to tbxAnalysisFolder (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    empty - handles not created until after all CreateFcns called
+
+    % Hint: edit controls usually have a white background on Windows.
+    %       See ISPC and COMPUTER.
+    if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+        set(hObject,'BackgroundColor','white');
+    end
 end
