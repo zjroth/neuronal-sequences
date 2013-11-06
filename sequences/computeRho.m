4b653b4256fdc6791742f14205318ba19276cc69
Zachary Roth <rothz@pastalkova-ws2.janelia.priv>
2013-11-06 13:06:59 -0500

    Optimize `computeRho`

--- sequences/computeRho.m
+++ sequences/computeRho.m
@@ -3,15 +3,13 @@ function dRho = computeRho(mtxMu1, mtxMu2, vCommonNeurons)
     % Restrict to the submatrices that correspond to the neurons that the
     % corresponding sequences share, and extract the upper-triangular portion
     % as a vector.
-    vMu1 = triuVals(mtxMu1(vCommonNeurons, vCommonNeurons), 1);
-    vMu2 = triuVals(mtxMu2(vCommonNeurons, vCommonNeurons), 1);
+    vMu1 = triu(mtxMu1(vCommonNeurons, vCommonNeurons), 1);
+    vMu2 = triu(mtxMu2(vCommonNeurons, vCommonNeurons), 1);
 
     % Now that the above restriction has been performed, the computation of rho
     % is straightforward.
-    dDotProduct = full(sum(vMu1 .* vMu2));
-    dNorm1 = sqrt(sum(vMu1 .* vMu1));
-    dNorm2 = sqrt(sum(vMu2 .* vMu2));
-    dDenom = dNorm1 * dNorm2;
+    dDotProduct = full(sum(sum(vMu1 .* vMu2)));
+    dDenom = sqrt(sum(sum(vMu1 .* vMu1)) * sum(sum(vMu2 .* vMu2)));
 
     % Account for the case that one of the restricted matrices is a zero-matrix.
     % This can happen even if the corresponding sequences have common neurons
