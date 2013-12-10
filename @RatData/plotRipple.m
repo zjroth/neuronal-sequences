9f46202c4baaef064d4c7315c7e2438cf89510d6
Zachary Roth <rothz@pastalkova-ws2.janelia.priv>
2013-12-10 16:11:04 -0500

    Remove `RatData` class

--- @RatData/plotRipple.m
+++
@@ -1,5 +0,0 @@
-% plotRipple(this, nRipple, varargin)
-function plotRipple(this, nRipple, varargin)
-    [strSection, nLocalRipple] = identifyRipple(this, nRipple);
-    plotRipple(this.(strSection), nLocalRipple, varargin{:});
-end
--- @RatData/sortNeuronsForRipple.m
+++
@@ -1,5 +0,0 @@
-% vOrder = sortNeuronsForRipple(this, nRipple, varargin)
-function vOrder = sortNeuronsForRipple(this, nRipple, varargin)
-    [strSection, nLocalRipple] = identifyRipple(this, nRipple);
-    vOrder = sortNeuronsForRipple(this.(strSection), nLocalRipple, varargin{:});
-end
