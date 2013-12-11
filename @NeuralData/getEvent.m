c68e63fc8f5eb3b8f498e3d312a7b6193cf1b767
Zachary Roth <rothz@pastalkova-ws2.janelia.priv>
2013-12-10 19:47:09 -0500

    Update `getEvent` to take classification parameter
    Also `getEvents`.

--- @NeuralData/getEvent.m
+++ @NeuralData/getEvent.m
@@ -1,5 +1,10 @@
 % objEvent = getEvent(this, vTimeWindow, strType)
 function objEvent = getEvent(this, vTimeWindow, strType)
     [vSequence, vTimes] = getSequence(this, vTimeWindow);
-    objEvent = Event(vTimeWindow, vTimes, vSequence);
+
+    if nargin == 3
+        objEvent = Event(vTimeWindow, vTimes, vSequence, strType);
+    else
+        objEvent = Event(vTimeWindow, vTimes, vSequence);
+    end
 end
--- @NeuralData/getEvents.m
+++ @NeuralData/getEvents.m
@@ -1,6 +1,21 @@
 % mtxEvents = getEvents(this)
-function cellEvents = getEvents(this, mtxTimeWindows)
-    for i = 1 : size(mtxTimeWindows, 1)
-        cellEvents{i} = getEvent(this, mtxTimeWindows(i, :));
+function cellEvents = getEvents(this, mtxTimeWindows, uknClassification)
+    if nargin < 3
+        uknClassification = 'none';
+    end
+
+    nWindows = size(mtxTimeWindows, 1);
+
+    if isa(uknClassification, 'cell')
+        assert(length(uknClassification) == nWindows, ...
+               'NeuralData.getEvents: classification parameter of incorrect size');
+        cellClassification = uknClassification;
+    elseif isa(uknClassification, 'char')
+        cellClassification = cell(nWindows, 1);
+        cellClassification(:) = {uknClassification};
+    end
+
+    for i = 1 : nWindows
+        cellEvents{i} = getEvent(this, mtxTimeWindows(i, :), uknClassification{i});
     end
 end
