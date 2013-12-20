d1ffba52e105d871d7d9ba10a470b418b849c7ec
Zachary Roth <rothz@pastalkova-ws2.janelia.priv>
2013-12-19 23:45:48 -0500

    Add `coactivityMatrix`

---
+++ sequences/coactivityMatrix.m
@@ -0,0 +1,40 @@
+%
+% USAGE:
+%
+%    [mtxCoactive, mtxActivityX, mtxActivityY] = coactivityMatrix(cellSeqsX, <cellSeqsY>)
+%
+% DESCRIPTION:
+%
+%    Find the coactivity matrix of a pair of sequence lists.
+%
+% ARGUMENTS:
+%
+%    cellSeqsX, <cellSeqsY>
+%
+%       The lists (cell arrays) of sequences. If the second of these is
+%       omitted, then it is set equal to the first.
+%
+% RETURNS:
+%
+%    mtxCoactive
+%
+%       The coactivity matrix. An the (i, j) entry of this matrix tells how many
+%       neurons sequence j of `cellSeqsX` has in common with sequence i of
+%       `cellSeqsY`.
+%
+%    mtxActivityX, mtxActivityY
+%
+%       The activity matrices (i.e., the return variables of `toMatrix`) of the
+%       input arguments since they have to be computed as an intermediate step
+%       of this computation.
+%
+function [mtxCoactive, mtxActivityX, mtxActivityY] = coactivityMatrix(cellSeqsX, cellSeqsY)
+    if nargin < 2
+        cellSeqsY = cellSeqsX;
+    end
+
+    nNeurons = maxActive([cellSeqsX(:); cellSeqsY(:)]);
+    mtxActivityX = toMatrix(cellSeqsX, nNeurons);
+    mtxActivityY = toMatrix(cellSeqsY, nNeurons);
+    mtxCoactive = mtxActivityY * mtxActivityX.';
+end
